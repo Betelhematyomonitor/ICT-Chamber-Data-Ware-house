46c64e4902982e94308d375ac75039d1724323c1
Betelhematyomonitor <betelhem@yomonitor.com>
2023-02-04 21:49:12 +0100

    Creating terrafform workflow github action

---
+++ gcs_bucket.tf
@@ -0,0 +1,5 @@
+resource "google_storage_bucket" "testing_ci_cd_pipeline_bucket" {
+      name = "testing_ci_cd_pipeline_bucket"
+      location = "EU"
+  
+}
---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "google" {
+     credentials =  "credential.json"
+     project =  "data-warehouse-dev-376508"
+     region =  "us-east1 (South Carolina)"
+    
+  
+}
+terraform {
+   backend "gcs"{
+    
+   }
+}
