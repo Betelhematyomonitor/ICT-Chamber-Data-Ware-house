77821fe85d8405ab6f3951daa579346e020a277a
Betelhematyomonitor <betelhem@yomonitor.com>
2023-02-04 22:30:24 +0100

    error fix

--- main.tf
+++ main.tf
@@ -2,11 +2,10 @@ provider "google" {
      credentials =  "credential.json"
      project =  "data-warehouse-dev-376508"
      region =  "us-east1 (South Carolina)"
-    
-  
 }
+
 terraform {
    backend "gcs"{
-         bucket = "ict_chamber_terraform_backend"
+         bucket = "tf_state_ict"
    }
 }
